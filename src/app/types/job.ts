f8d58031d4183db8f44c0435dd445be1b42e02f6
sam <me@sammason.dev>
2026-04-29 13:33:39 -0400

    implement basic typestate foundation

---
+++ src/app/types/job.ts
@@ -0,0 +1,21 @@
+export type JobState = IdleState | PreppingState | TransitState | CompletedState;
+
+export type IdleState = {
+  type: 'IDLE';
+};
+
+export type PreppingState = {
+  type: 'PREPPING';
+  prepStartTime: number;
+};
+
+export type TransitState = {
+  type: 'TRANSIT';
+  itemsChecked: boolean;
+  transitStartTime: number;
+};
+
+export type CompletedState = {
+  type: 'COMPLETED';
+  completedTime: number;
+};
