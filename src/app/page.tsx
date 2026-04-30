09260aa2b405479c398d33f71608e21cda1da98f
sam <me@sammason.dev>
2026-04-30 14:31:15 -0400

    restore zustand tutorial learning state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-import Image from 'next/image';
+'use client';
+
+import { useCounterStore } from '@/store';
 
 export default function Home() {
+  return <OtherComponent />;
+}
+
+function OtherComponent() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
   return (
-    <div className="grid place-items-center h-screen text-4xl">Logistic</div>
+    <div className="flex items-center justify-center gap-4 h-screen">
+      <div className="flex items-center justify-center gap-4 h-32">
+        <button
+          onClick={() => decrement()}
+          className="h-full w-32 px-4 rounded-tr-2xl rounded-bl-2xl text-4xl bg-amber-900 cursor-pointer"
+        >
+          -
+        </button>
+        <span className="flex items-center justify-center h-full w-32 rounded-t-2xl text-2xl bg-amber-950">
+          {count}
+        </span>
+        <button
+          onClick={() => increment()}
+          className="h-full w-32 px-4 rounded-tl-2xl rounded-br-2xl text-4xl bg-amber-600 cursor-pointer"
+        >
+          +
+        </button>
+      </div>
+    </div>
   );
 }
